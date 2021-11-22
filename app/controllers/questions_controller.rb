5d77ac7df205ed3c8708f37d81a30e2d5ae944da
Claudia Manchon <claudiambomb@gmail.com>
2021-11-22 12:44:28 +0800

    Stupid Coaching with Rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+
+  end
+
+  def answer
+    @coach_answer = ''
+    @user_answer = params[:answer].to_s
+    if @user_answer[-1] == '?'
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    elsif @user_answer == 'I am going to work'
+      @coach_answer = 'Great!'
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
